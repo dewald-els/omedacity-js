63f557328db93581d193034b2abda722d1b34004
Dewald Els <dewaldifels@gmail.com>
2023-12-27 14:02:34 +0100

    feat: Refactor heroes api/collection

--- src/api/heroes.ts
+++ src/api/heroes.ts
@@ -7,21 +7,19 @@ import { Heroes, Hero } from "../types";
  * @param {Axios} client
  * @returns
  */
-const heroesFindAll = (client: Axios) => (): Promise<Heroes> => {
+export const heroesFindAll = (client: Axios) => (): Promise<Heroes> => {
   return client.get<Heroes>("/heroes.json").then((response: AxiosResponse) => {
     const { data } = response;
     return data;
   });
 };
 
-export const findHeroesWithClient = heroesFindAll(client);
-
 /**
  * Find Hero by name
  * @param {Axios} client
  * @returns
  */
-const heroesFindByName = (client: Axios) => (heroName: string) => {
+export const heroesFindByName = (client: Axios) => (heroName: string) => {
   return client
     .get<Hero>("/heroes/" + heroName + ".json")
     .then((response: AxiosResponse) => {
@@ -29,5 +27,3 @@ const heroesFindByName = (client: Axios) => (heroName: string) => {
       return data;
     });
 };
-
-export const findHeroByNameWithClient = heroesFindByName(client);
